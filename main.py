b9ee1125639919800ee5983986ce269a49de56f5
xiaoyang.yx <xiaoyang.yx@xuelangyun.com>
2022-04-28 18:12:03 +0800

    update

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+import pandas as pd
 from libtlda.flda import FeatureLevelDomainAdaptiveClassifier
 
 
 def run():
-    ...
+    src = pd.read_csv('./source_data_feature.csv')
+    src_label = pd.read_csv('./source_data_label.csv')
+    dst = pd.read_csv('./dst_data_feature.csv')
+
+    cls = FeatureLevelDomainAdaptiveClassifier()
+    cls.fit(src,src_label,dst)
+    
+    
 
 
 
